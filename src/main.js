8d84e8943ec18df6546dd9e0e7fb40ffef25baef
Iryna Shenenko <163681074+Iryna-Shenenko@users.noreply.github.com>
2024-10-13 11:40:33 -0600

    add fix-conteine8

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import './js/my_project';
 import './js/acardion';
 import './js/body-theme';
 import './js/menu';
-import '/js/work-together';
+import './js/work-together';
+import './js/fetch-reviews';
