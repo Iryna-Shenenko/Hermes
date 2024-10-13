bf52c25f5728bbc3be5f8ce52e05e0184b721fa6
Iryna Shenenko <163681074+Iryna-Shenenko@users.noreply.github.com>
2024-10-13 12:42:33 -0600

    add fix-reviews

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import "./js/scroll";
-import "./js/my_project";
-import "./js/acardion";
-import "./js/fetch-reviews";
-import "./js/body-theme";
-import "./js/menu";
+import './js/scroll';
+import './js/my_project';
+import './js/acardion';
+import './js/fetch-reviews';
+import './js/body-theme';
+import './js/menu';
+
