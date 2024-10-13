d726f6ac8f00c2a11ef283f9f8d71567d67d07e5
maksymgulko <maksymgulko@gmail.com>
2024-10-13 11:39:49 +0200

    push

--- src/js/fetch-reviews.js
+++ src/js/fetch-reviews.js
@@ -10,8 +10,8 @@ const carousel = document.querySelector(".reviews-list");
 const swiper = new Swiper(".swiper", {
   modules: [Navigation, Pagination],
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".nav-arrow-next",
+    prevEl: ".nav-arrow-prev",
   },
 });
 
@@ -26,13 +26,16 @@ async function createMarkup(reviews) {
     return reviews.data
       .map((comment) => {
         const { author, avatar_url, review } = comment;
-        return `<li class="swiper-slide">
-  <p>${review}</p>
-  <div>
-  <img src="${avatar_url}">
-  <p>${author}</p>
-  </div>
-  </li>`;
+        return `
+        <li class="swiper-slide">
+        <div class="review-item">
+        <p class="review-text">${review}</p>
+        <div class="review-credentials">
+          <img class="review-img" src="${avatar_url}" />
+          <p class="review-author">${author}</p>
+          </div>
+        </div>
+      </li>`;
       })
       .join("");
   } catch (error) {
@@ -41,25 +44,23 @@ async function createMarkup(reviews) {
 }
 
 const breakpoints = {
-  mobile: 480,
+  mobile: 320,
   tablet: 768,
-  desktop: 1024,
+  desktop: 1280,
 };
 
 function handleViewportChange() {
   const currentWidth = window.innerWidth;
-
   if (currentWidth < breakpoints.mobile) {
     swiper.params.slidesPerView = 1;
-
     // Mobile layout
   } else if (currentWidth < breakpoints.tablet) {
     swiper.params.slidesPerView = 1;
-
     // Tablet layout
-  } else {
+  } else if (currentWidth > breakpoints.desktop) {
     // Desktop layout
     swiper.params.slidesPerView = 2;
+    swiper.update();
   }
 }
 
