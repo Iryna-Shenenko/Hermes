b4ed4fefe2f5a7f08b4a73016b7fd4a0aaafa694
maqss <maksymgulko@gmail.com>
2024-10-13 17:19:13 +0200

    functional reviews slider

--- src/js/fetch-reviews.js
+++ src/js/fetch-reviews.js
@@ -7,6 +7,12 @@ import "swiper/css/pagination";
 
 const carousel = document.querySelector(".reviews-list");
 
+const breakpoints = {
+  mobile: 320,
+  tablet: 768,
+  desktop: 1280,
+};
+
 const swiper = new Swiper(".swiper", {
   modules: [Navigation, Pagination],
   navigation: {
@@ -14,6 +20,9 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".nav-arrow-prev",
   },
 });
+
+handleViewportChange();
+window.addEventListener("resize", handleViewportChange);
 
 const response = await axios.get(
   "https://portfolio-js.b.goit.study/api/reviews"
@@ -27,12 +36,12 @@ async function createMarkup(reviews) {
       .map((comment) => {
         const { author, avatar_url, review } = comment;
         return `
-        <li class="swiper-slide">
-        <div class="review-item">
-        <p class="review-text">${review}</p>
-        <div class="review-credentials">
-          <img class="review-img" src="${avatar_url}" />
-          <p class="review-author">${author}</p>
+        <li class="swiper-slide reviews-wrap">
+        <div class="reviews-item">
+        <p class="reviews-text">${review}</p>
+        <div class="reviews-credentials">
+          <img class="reviews-img" src="${avatar_url}" />
+          <p class="reviews-author">${author}</p>
           </div>
         </div>
       </li>`;
@@ -43,25 +52,28 @@ async function createMarkup(reviews) {
   }
 }
 
-const breakpoints = {
-  mobile: 320,
-  tablet: 768,
-  desktop: 1280,
-};
-
 function handleViewportChange() {
   const currentWidth = window.innerWidth;
-  if (currentWidth < breakpoints.mobile) {
+  if (currentWidth < breakpoints.tablet) {
     swiper.params.slidesPerView = 1;
+    swiper.update();
+    console.log("mobile");
+
     // Mobile layout
-  } else if (currentWidth < breakpoints.tablet) {
+  } else if (
+    currentWidth > breakpoints.tablet &&
+    currentWidth < breakpoints.desktop
+  ) {
     swiper.params.slidesPerView = 1;
+    swiper.update();
+    console.log("tablet");
+
     // Tablet layout
-  } else if (currentWidth > breakpoints.desktop) {
+  } else {
     // Desktop layout
     swiper.params.slidesPerView = 2;
+    swiper.params.spaceBetween = 24;
     swiper.update();
+    console.log("desktop");
   }
 }
-
-window.addEventListener("resize", handleViewportChange);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./js/scroll";
 import "./js/my_project";
 import "./js/acardion";
+import "./js/fetch-reviews";
 import "./js/body-theme";
 import "./js/menu";
